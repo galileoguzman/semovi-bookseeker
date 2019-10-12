44f5835e7d99960496e941e834dfc2312b013d31
galileoguzman <galileoguzman@gmail.com>
2019-10-12 16:30:40 -0500

    Logout

--- app/src/main/java/com/galileoguzman/bookseeker/MainActivity.java
+++ app/src/main/java/com/galileoguzman/bookseeker/MainActivity.java
@@ -2,13 +2,30 @@ package com.galileoguzman.bookseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.galileoguzman.bookseeker.utils.DataProcessor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DataProcessor dataProcessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataProcessor = new DataProcessor(this);
+    }
+
+    public void btnLogoutPressed(View view) {
+        dataProcessor.saveStringValueForKey("userToken", "---");
+        dataProcessor.saveBooleanValueForKey("isActiveSession", false);
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
