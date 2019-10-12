e08ab189dd95f194f47dbc337ca490417d66ce91
galileoguzman <galileoguzman@gmail.com>
2019-10-12 14:43:58 -0500

    Added logic to initialize activities

--- app/src/main/java/com/galileoguzman/bookseeker/SplashActivity.java
+++ app/src/main/java/com/galileoguzman/bookseeker/SplashActivity.java
@@ -2,6 +2,7 @@ package com.galileoguzman.bookseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Boolean isActiveSession = true;
+        final Intent intent;
+
+        if (isActiveSession) {
+            intent = new Intent(SplashActivity.this, MainActivity.class);
+        } else {
+            intent = new Intent(SplashActivity.this, LoginActivity.class);
+        }
+        startActivity(intent);
     }
 }
